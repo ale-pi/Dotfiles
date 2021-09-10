468937c9227a77708d87c8e38e49366c42d1f022
Alessandro Perlo <aleperlo002@protonmail.com>
2021-09-10 10:46:44 +0200

    Added vscode and pycharm repositories

--- setup.sh
+++ setup.sh
@@ -4,5 +4,10 @@
 sudo dnf install ansible stow
 # Enable RPM fusion repository
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Enable vscode repository
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# Enable pycharm copr
+dnf copr enable phracek/PyCharm
 # Update
 sudo dnf update
