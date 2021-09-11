c81034c13947d787c50fcfa2ae57f51875616c89
Alessandro Perlo <aleperlo002@protonmail.com>
2021-09-11 12:43:38 +0200

    Added missing sudo

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 # Enable pycharm copr
-dnf copr enable phracek/PyCharm
+sudo dnf copr enable phracek/PyCharm
 # Update
 sudo dnf update
