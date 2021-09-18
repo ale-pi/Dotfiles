5811d49aa9a12f3cff7507b86ba81149a9292f55
Alessandro Perlo <aleperlo002@protonmail.com>
2021-09-18 16:06:36 +0200

    Updated packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-themes eshell-syntax-highlighting eshell-git-prompt emms geiser-guile geiser smartparens yasnippet-snippets yaml-mode markdown-mode eglot magit company embark-consult embark marginalia consult orderless vertico which-key org-bullets visual-fill-column minions color-theme-sanityinc-tomorrow dired-rainbow diredfl dired-subtree avy use-package)))
+   '(eshell-syntax-highlighting eshell-git-prompt emms geiser-guile geiser smartparens yasnippet-snippets yaml-mode markdown-mode eglot magit company embark-consult embark marginalia consult orderless vertico which-key org-bullets visual-fill-column minions color-theme-sanityinc-tomorrow dired-rainbow diredfl dired-subtree avy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
